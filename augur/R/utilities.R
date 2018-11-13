bd5c4ced2d08bd8b7e2af9f5f4d26624a75eb37f
Mustafa Ascha <mustafa.ascha@gmail.com>
2018-11-13 12:55:39 -0500

    Update utilities.R

--- augur/R/utilities.R
+++ augur/R/utilities.R
@@ -1,6 +1,7 @@
 #' Return the matching regex
 #' 
-#' I suspect there's a better way to do this.
+#' I suspect there's a better way to do this. 
+#' Edit: Months later - Indeed, there's an argument 'value' to 'grep' that will return a match
 #' 
 #' @param x The thing to examine
 #' @param p The pattern to grep
